72ddfdd0bbfa39d6d06af33c67b17ba13f6c7230
calmpsycho333 <wrathking75@yahoo.com>
2020-12-11 11:15:26 -0500

    commit tree b61b991ea7d1e287c295f2b921f86d7b3189f53b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require 'pry'
+def countdown(number) 
+   # binding.pry# <-- number is 10
+    while  number > 0
+    
+   puts "#{number} SECOND(S)!"
+   number  -= 1
+       
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(num)
+    sleep num 
+        
 end
